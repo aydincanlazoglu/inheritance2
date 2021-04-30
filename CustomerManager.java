8b82aa3487ad8397f36399b0c7122da29019098f
aydincanlazoglu <83122261+aydincanlazoglu@users.noreply.github.com>
2021-05-01 00:56:02 +0300

    Update CustomerManager.java

--- CustomerManager.java
+++ CustomerManager.java
@@ -2,7 +2,7 @@ package inheritance2;
 
 public class CustomerManager {
 	public void add(EmailLogger emailLogger) {
-		System.out.println("Müþteri Eklendi");
+		System.out.println("MÃ¼ÅŸteri Eklendi");
 		DatabaseLogger logger = new DatabaseLogger();
 		EmailLogger logger1 = new EmailLogger();
 		FileLogger logger2 = new FileLogger();
