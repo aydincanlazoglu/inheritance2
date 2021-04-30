c23f75a2c9c39b79c9dacf1923f2d5e7dc53305f
aydincanlazoglu <83122261+aydincanlazoglu@users.noreply.github.com>
2021-05-01 00:54:47 +0300

    Update FileLogger.java

--- FileLogger.java
+++ FileLogger.java
@@ -3,6 +3,6 @@ package inheritance2;
 public class FileLogger extends Logger{
 	@Override
 	public void log( ) {
-		System.out.println("Dosya Longlandý	");
+		System.out.println("Dosya LonglandÄ±");
  }
 }
